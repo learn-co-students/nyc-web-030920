cb8d259dd2ee5e08b9bda68915e3f1a1c3a6b86b
github username <github email address>
2020-03-09 17:26:33 -0400

    hashketball

---
+++ 01-hashketball-review/README.md
@@ -0,0 +1,28 @@
+
+## What to Expect
+
+- Calendars/Events
+- Coaches
+  - day to day help
+- Instructors
+  - lectures/office hours/when available
+- Main Goal
+  - Independent problem solver
+  - To learn not just remember
+- Labs
+  - Do them
+- Lectures
+  - participation
+  - taking notes
+
+## Objectives
+
+- What is TDD
+- irb
+- binding.pry
+- helper methods
+- Iterators
+  - each
+  - map/collect
+  - select
+  - find
---
+++ 01-hashketball-review/hashketball.rb
@@ -0,0 +1,153 @@
+require 'pry'
+
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        { name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        { name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        { name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+         { name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        { name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        { name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        { name: "Bismak Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+         { name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        { name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        { name: "Brendan Haywood",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
+
+# .each
+  # - returns the original data 
+# .map/.collect
+  # - returns a new array
+# .find
+  # - return the first element that fits the condition
+# .select
+  # - returns an array of all of the elements that fit the condition 
+
+
+def players
+  players_array = []
+  game_hash.each do |k,v|
+    players_array << v[:players]
+  end
+  players_array.flatten
+# iterate through the game_hash and returns a list of all the players
+end
+
+
+def find_player(name)
+  players.find do |player_hash|
+    player_hash[:name] == name
+  end
+  # iterate through my players to see which hash in the players array has a "name" key that equals the name argument
+end
+
+def num_points_scored(name)
+  find_player(name)[:points]
+  # return the "points" key for that specific player 
+
+end
+
+puts num_points_scored("DeSagna Diop")
