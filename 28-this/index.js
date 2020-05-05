ec9d47deafac476adcd9ee268ec7a362a8715805
Steven Doran <steven.doran@flatironschool.com>
2020-05-05 10:28:07 -0400

    pre lecture

---
+++ 28-this/index.js
@@ -0,0 +1,22 @@
+console.log("this is so much fun!")
+
+/************ Simple Function Call ***********************/
+
+
+
+/************ Bind/Call/Apply ****************************/
+
+
+
+/************ Function called on an Object ***************/
+
+
+
+
+/************ Function Called with New Keyword ***********/
+
+
+
+/************ Arrow Functions ****************************/
+
+
